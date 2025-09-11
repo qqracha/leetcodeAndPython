378256c9203a1c4fe29b36261a4e43ab9df8e368
Ruberoidd <RuberoidMuted@gmail.com>
2025-09-11 21:52:51 +0200

    Update 217.ContainsDuplicate.py

--- solutions/217.ContainsDuplicate.py
+++ solutions/217.ContainsDuplicate.py
@@ -2,10 +2,26 @@ from typing import Optional, List
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        clear = {}
+        clear = set(nums)
+        if len(clear) == len(nums):
+            return False
+        else:
+            return True
 
 
 
 
 sol = Solution()
-print(sol.containsDuplicate())
+print(sol.containsDuplicate([1, 2, 3 ,1]))
+
+
+# big_list = [1,2,3,1]
+# big_set = set(big_list)
+# def smart():
+#     if len(big_set) == len(big_list):
+#         return False
+#     else:
+#         return True
+
+# mem = smart
+# print(mem)
