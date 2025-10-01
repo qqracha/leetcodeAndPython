d99a081bf829ecdf23cd3f6270f9a20118a32c14
qqracha <evaldyukhov@gmail.com>
2025-10-01 13:24:35 +0200

    hash

--- notes/main.py
+++ notes/main.py
@@ -1,6 +1,12 @@
 s = [1,2,3,4,5,6,15,7,8]
+strigTest = "goofyAH"
+stringTest_copy = strigTest # хэши будут одинаковыми
 
 print(s[:2]) # [0,1]
 print(s[2:]) # [2,3,4,5,6,15,7,8]
 print(s[2:5]) # [3,4,5]
 print(s.index(15)) # 6
+print(s.count(5))
+
+print(hash(strigTest))
+print(hash(stringTest_copy))
