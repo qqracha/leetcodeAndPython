9d40608121994c8f5385d7c1e15fed0cb62de59d
qqracha <evaldyukhov@gmail.com>
2025-10-02 20:52:41 +0200

    zip

--- notes/dict_fromkeys.py
+++ notes/dict_fromkeys.py
@@ -1,7 +1,10 @@
 words = ["apple", "banana", "cherry"]
+vals = [1, 2, 3]
 d = dict.fromkeys(words, 95)   # all keys have 95 value
 for key, value in d.items():
     print(key, value)
 
-d.popitem()
+d.values()
 print(d)
+
+print(dict(zip(words, vals)))
