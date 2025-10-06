c2dcdfd5e9ac26e6e417c0c8c4650aa6323463b5
qqracha <evaldyukhov@gmail.com>
2025-10-06 14:04:29 +0200

    add com args

--- notes/args.py
+++ notes/args.py
@@ -1,4 +1,7 @@
-def greet(*args):
+# Args & Kwargs - именнованные и неименованные аргументы, при помощи которых можно создавать фукнции, которые принимают 
+# количество аргументов
+
+def greet(*args): # *args - в кортеж
     print(args)
 
 greet("i", "wanna", "pizza")
@@ -10,7 +13,7 @@ print(my_sum(1, 2))           # 3
 print(my_sum(1, 2, 3, 4, 5))  # 15
 
 
-def info(**kwargs):
+def info(**kwargs): # **kwargs - в словарь
     print(kwargs)
 
 info(name = "Даша", age = 20)
