fd84ddbbdc3ee6882858371a530abdbeedca2f6c
qqracha <evaldyukhov@gmail.com>
2025-10-06 14:47:58 +0200

    gens with .send & .throw & .close

--- notes/yield.py
+++ notes/yield.py
@@ -16,3 +16,35 @@ gen = count_up_to(10)
 # Получаем значение по одному
 for number in gen:
     print(number)
+
+# Пример генератора с .send и возведением в квадрат
+def generator_example():
+    value = yield #  точка ожидания внешнего значения
+    yield value ** 2
+
+gen_e = generator_example()
+
+next(gen_e)
+print(gen_e.send(10))
+
+# Пример генератора с ошибкой
+def generator_with_exception():
+    try:
+        yield
+    except ValueError:
+        yield "Oops.."
+
+gen_error = generator_with_exception()
+next(gen_error) # Запускаем оператор до первого yield
+print(gen_error.throw(ValueError)) # Выбрасывает исключение внутри генератора
+
+# Пример генератора с методом .close
+def generator_with_close():
+    try:
+        yield 1
+    except GeneratorExit:
+        print("Generator was ended!")
+
+gen_close = generator_with_close()
+next(gen_close)
+gen_close.close()
